fa6e6261492c4041a06a170e9835b5acab2dfbda
bkuma144 <bipul.kumar1@aexp.com>
2020-06-20 09:36:56 +0530

    intital commit "streaming files"

---
+++ src/main/java/com/amex/amvcafe/videostream/Application.java
@@ -0,0 +1,12 @@
+package com.amex.amvcafe.videostream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
